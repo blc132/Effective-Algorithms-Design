c1b5654c70758e52e25464c121299c8679dd0f17
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-29 22:41:18 +0100

    Uproszczenie pozornej nieskonczonosci

--- helpers.py
+++ helpers.py
@@ -9,10 +9,6 @@ def ptype(value):
     print(type(value))
 
 
-def lets_say_its_inf():
-    return 9999999
-
-
 def get_all_ints_from_string(value):
     array = []
     for t in value.split():
@@ -21,3 +17,6 @@ def get_all_ints_from_string(value):
         except ValueError:
             pass
     return array
+
+
+INF = 9999999
