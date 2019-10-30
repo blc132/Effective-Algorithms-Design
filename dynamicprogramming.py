7c716b77fb3a3565626de71b44842855dd97b42d
Paweł Kobryński <pawelkobrynski97@gmail.com>
2019-10-30 12:55:31 +0100

    Dodanie komentarzy do dp

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -20,10 +20,12 @@ class DynamicProgramming(Graph):
         self.cost_matrix = graph.cost_matrix
         self.file_name = graph.file_name
         self.number_of_cities = graph.number_of_cities
-
+        # maska bitowa wszystkich odwiedzonych miast (2^n)-1
         self.all_visited_vertices_mask = (1 << self.number_of_cities) - 1
+        # dla każdego miasta oddzielna maska, czyli tablica dwuwymiarowa [n, (2^n)-1]
         self.all_vertices_subsets = numpy.full((self.number_of_cities, 1 << self.number_of_cities), INF, dtype=int)
         self.sub_path_indexes = numpy.full((self.number_of_cities, 1 << self.number_of_cities), INF, dtype=int)
+        self.route.clear()
 
         for x in range(self.number_of_cities):
             for y in range(self.all_visited_vertices_mask + 1):
@@ -31,11 +33,16 @@ class DynamicProgramming(Graph):
                 self.sub_path_indexes[x, y] = INF
 
     def start(self, starting_vertex):
+        # przypisanie starowego wierzchołka
         self.starting_vertex = starting_vertex
-        current_state_of_vertices = 1 << starting_vertex
-        self.best_cycle_cost = self.__start(starting_vertex, current_state_of_vertices)
-        index = starting_vertex
 
+        # dopisanie do maski bitowej startowego wierzchołka, w domyśle to po prostu 000...01
+        current_state_of_vertices = 1 << starting_vertex
+
+        self.best_cycle_cost = self.__start(starting_vertex, current_state_of_vertices)
+
+        index = starting_vertex
+        # przepisanie indeksów na ścieżkę grafu
         for x in range(self.number_of_cities):
             self.route.append(index)
             next_index = self.sub_path_indexes[index, current_state_of_vertices]
@@ -45,9 +52,12 @@ class DynamicProgramming(Graph):
         self.route.append(starting_vertex)
 
     def __start(self, current_vertex, current_vertex_state_mask):
+
+        # jeśli maska == 11..11, zwróć koszt pomiędzy aktualnym wierzchołkiem a pierwszym
         if current_vertex_state_mask == self.all_visited_vertices_mask:
             return self.cost_matrix[current_vertex, self.starting_vertex]
 
+        # jeśli maska została wczeniej obliczona to zwróć ją
         if self.all_vertices_subsets[current_vertex, current_vertex_state_mask] != INF:
             return self.all_vertices_subsets[current_vertex, current_vertex_state_mask]
 
@@ -55,9 +65,10 @@ class DynamicProgramming(Graph):
         current_vertex_index: int = -1
 
         for x in range(self.number_of_cities):
-            # jesli zostalo odwiedzone
+            # jesli zostalo odwiedzone, czyli AND == 1
             if current_vertex_state_mask & (1 << x) != 0:
                 continue
+            # kolejna maska to będzie aktualna OR maska startowa wierzchołka x
             next_state_mask = current_vertex_state_mask | (1 << x)
             new_cost_of_travel = self.cost_matrix[current_vertex, x] + self.__start(x, next_state_mask)
 
